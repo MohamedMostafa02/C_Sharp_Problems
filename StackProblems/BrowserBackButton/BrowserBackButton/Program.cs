aa3f01b3ad85ee0bb1aeb5f711a536f98cbdb7e6
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-01 02:49:35 +0300

    Add Browser Back Button Problem

---
+++ StackProblems/BrowserBackButton/BrowserBackButton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserBackButton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> history = new Stack<string>();
+
+            history.Push("Page1");
+            history.Push("Page2");
+            history.Push("Page3");
+
+            Console.WriteLine("Back From: "+ history.Pop()); // output: back from page 3
+            Console.WriteLine("Current Page: "+ history.Peek()); 
+            Console.ReadKey();
+        }
+    }
+}
