c66ec2ff82d89ca2b42db1ccf33575c241df7306
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-01 02:57:55 +0300

    Add Decimal To Binary Problem

---
+++ StackProblems/ConvertDecimalToBinary/ConvertDecimalToBinary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDecimalToBinary
+{
+    internal class Program
+    {
+        static string DecimalToBinary(int number)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            while(number > 0)
+            {
+                stack.Push(number % 2);
+                number /= 2;
+            }
+
+            return string.Join("", stack);
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DecimalToBinary(10)); //output : 1010 
+            Console.ReadKey();
+
+
+        }
+    }
+}
