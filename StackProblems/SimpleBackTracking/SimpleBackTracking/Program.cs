b85201653689c951cc1f1cfa95b22bc627ca9c79
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-01 19:50:13 +0300

    Add Simple BackTracking Problem

---
+++ StackProblems/SimpleBackTracking/SimpleBackTracking/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBackTracking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a stack to store the path or visited locations
+            Stack<string> stack = new Stack<string>();
+
+            // Push locations to the stack (like a path followed by a person)
+            stack.Push("Start");
+            stack.Push("Go To Gaz Station");
+            stack.Push("Go To Super Market");
+            stack.Push("Go To Work");
+            stack.Push("Go To Cafe");
+            stack.Push("Go Home");
+
+            Console.WriteLine("BackTracking.....\n");
+
+            // Loop through the stack (from top to bottom) to simulate backtracking
+            foreach (var item in stack)
+            {
+                Console.WriteLine($"Back to: {item}");
+            }
+
+            Console.ReadKey(); // Wait for a key press before closing
+        }
+    }
+}
