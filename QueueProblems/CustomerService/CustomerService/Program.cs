8ccc4f9bac2596546823b1181934b01dd4d465d3
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-01 21:01:34 +0300

    Add Customer Service Problem

---
+++ QueueProblems/CustomerService/CustomerService/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a queue to store customer names
+            Queue<string> customers = new Queue<string>();
+
+            // Enqueue customers into the queue
+            customers.Enqueue("Customer1");
+            customers.Enqueue("Customer2");
+            customers.Enqueue("Customer3");
+
+            // Display the start of customer service
+            Console.WriteLine("Serving Customers:\n");
+
+            // Serve customers while the queue is not empty
+            while (customers.Count > 0)
+            {
+                // Dequeue the next customer (FIFO order)
+                string customer = customers.Dequeue();
+
+                // Display the customer being served
+                Console.WriteLine($"Serving: {customer}");
+            }
+
+            // Wait for a key press before closing the console
+            Console.ReadKey();
+        }
+    }
+}
