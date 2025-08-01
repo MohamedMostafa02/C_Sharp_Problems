71f0d02126919f0b99c4249669a7f63e68662562
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-01 03:06:56 +0300

    Add Undo Operations In Calculator

---
+++ StackProblems/UndoOperationsInCalculator/UndoOperationsInCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndoOperationsInCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a stack to store results
+            Stack<int> CalculatorStack = new Stack<int>();
+
+            // Add some values to the stack
+            CalculatorStack.Push(10);
+            CalculatorStack.Push(20);
+            CalculatorStack.Push(30);
+
+            // Undo last operation (remove the last value)
+            Console.WriteLine("Undo: " + CalculatorStack.Pop());
+
+            // Show the current result (top value in the stack)
+            Console.WriteLine("Current Result: " + CalculatorStack.Peek());
+
+            // Wait for key press to close the program
+            Console.ReadKey();
+        }
+    }
+}
