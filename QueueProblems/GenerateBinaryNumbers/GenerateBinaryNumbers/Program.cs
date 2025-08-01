638aa30e6b1d81cf299bc606b150f57464911419
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-01 21:50:51 +0300

    Add Generate Binary Numbers Problem

---
+++ QueueProblems/GenerateBinaryNumbers/GenerateBinaryNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateBinaryNumbers
+{
+    internal class Program
+    {
+        // Method to generate the first 'n' binary numbers using a queue (BFS-like approach)
+        static void GenerateBinaryNumbers(int n)
+        {
+            // check the n is more than 0
+            if (n <= 0)
+            {
+                Console.WriteLine("No binary numbers to generate.");
+                return;
+            }
+
+            // Create a queue to hold binary numbers
+            Queue<string> queue = new Queue<string>();
+
+            // Start by enqueueing the first binary number "1"
+            queue.Enqueue("1");
+
+            // Generate 'n' binary numbers
+            for (int i = 0; i < n; i++)
+            {
+                // Dequeue the front element
+                string binary = queue.Dequeue();
+
+                // Print the current binary number
+                Console.WriteLine(binary);
+
+                // Enqueue the next two binary numbers by appending "0" and "1"
+                queue.Enqueue(binary + "0");
+                queue.Enqueue(binary + "1");
+            }
+
+            // Clear the queue after use
+            queue.Clear();
+        }
+
+        static void Main(string[] args)
+        {
+            // Call the method to generate binary numbers from 1 to 5
+            GenerateBinaryNumbers(5);
+
+            Console.ReadKey(); // Wait for key press before closing
+        }
+    }
+}
---
+++ QueueProblems/GenerateBinaryNumbers/GenerateBinaryNumbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GenerateBinaryNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenerateBinaryNumbers")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff1352dd-4d79-4c91-899c-0bf73cfacc9d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
