f9d724298226a61bb1722fbf8b4c495f27c21dee
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-02 22:36:54 +0300

    Add Clone Queue Using Recursion Problem

---
+++ QueueProblems/CloneQueueUsingRecursion/CloneQueueUsingRecursion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneQueueUsingRecursion
+{
+    internal class Program
+    {
+        // Recursive method to clone a queue without using extra space (except recursion stack)
+        static Queue<int> CloneQueue(Queue<int> queue)
+        {
+            // Base case: if the queue is empty, return a new empty queue
+            if (queue.Count == 0) return new Queue<int>();
+
+            // Remove the front element
+            int front = queue.Dequeue();
+
+            // Recursively clone the remaining queue
+            Queue<int> clone = CloneQueue(queue);
+
+            // After recursion: put the front element back in the original queue
+            queue.Enqueue(front);
+
+            // Also enqueue the element into the cloned queue
+            clone.Enqueue(front);
+
+            // Return the cloned queue
+            return clone;
+        }
+
+        static void Main(string[] args)
+        {
+            // Initialize the original queue with sample values
+            Queue<int> queue = new Queue<int>(new[] { 1, 2, 3, 4 });
+
+            // Call the recursive clone function
+            Queue<int> clonedQueue = CloneQueue(queue);
+
+            // Display both the cloned and original queues
+            Console.WriteLine("Cloned Queue: " + string.Join(", ", clonedQueue));
+            Console.WriteLine("Original Queue: " + string.Join(", ", queue));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QueueProblems/CloneQueueUsingRecursion/CloneQueueUsingRecursion/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CloneQueueUsingRecursion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CloneQueueUsingRecursion")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f23c2cd-e6d7-4628-a936-e851f5d9c3b2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
