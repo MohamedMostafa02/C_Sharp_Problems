bde98643e74b56fd83cec7e861bf25695cc6aec4
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-04 01:18:24 +0300

    Add Track Unique Visitors Problem

---
+++ HashSetProblems/TrackUniqueVisitors/TrackUniqueVisitors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackUniqueVisitors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a HashSet to store unique visitor IP addresses
+            // HashSet automatically prevents duplicates
+            HashSet<string> uniqueVisitors = new HashSet<string>();
+
+            // Add IP addresses to the HashSet
+            uniqueVisitors.Add("192.168.1.1"); // first visitor
+            uniqueVisitors.Add("192.168.1.2"); // second visitor
+            uniqueVisitors.Add("192.168.1.2"); // duplicate, will not be added again
+
+            // Display the number of unique visitors
+            Console.WriteLine("Unique Visitors: " + uniqueVisitors.Count);
+
+            // Wait for a key press before closing the console window
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HashSetProblems/TrackUniqueVisitors/TrackUniqueVisitors/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TrackUniqueVisitors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrackUniqueVisitors")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c5b46e04-89e5-43ba-84a3-155d8b7d9451")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
