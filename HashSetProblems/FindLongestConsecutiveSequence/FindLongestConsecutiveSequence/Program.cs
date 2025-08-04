434eab46648dd1a92b66ce81e4556f246f053fa2
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-04 03:04:55 +0300

    Add Find Longest Consecutive Sequence Problem

---
+++ HashSetProblems/FindLongestConsecutiveSequence/FindLongestConsecutiveSequence/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindLongestConsecutiveSequence
+{
+    internal class Program
+    {
+        // Method 1: Efficient approach using HashSet (O(n) time)
+        static int LongestConsecutive(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            // Use HashSet for O(1) lookup times
+            HashSet<int> set = new HashSet<int>(nums);
+
+            int longestStreak = 0;
+
+            foreach (int num in set)
+            {
+                // Only start counting if it's the beginning of a sequence
+                if (!set.Contains(num - 1))
+                {
+                    int currentNum = num;
+                    int currentStreak = 1;
+
+                    // Keep incrementing as long as the next number exists in the set
+                    while (set.Contains(currentNum + 1))
+                    {
+                        currentNum++;
+                        currentStreak++;
+                    }
+
+                    // Update the maximum streak found so far
+                    longestStreak = Math.Max(longestStreak, currentStreak);
+                }
+            }
+
+            return longestStreak;
+        }
+
+        // Method 2: Simpler approach using sorting (O(n log n) time)
+        static int LongestConsecutive2(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            // Sort the array
+            Array.Sort(nums);
+
+            int longestStreak = 1;
+            int currentStreak = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                // Skip duplicates
+                if (nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                // If current number is consecutive to previous, increase streak
+                else if (nums[i] == nums[i - 1] + 1)
+                {
+                    currentStreak++;
+                }
+                // Not consecutive, reset streak
+                else
+                {
+                    currentStreak = 1;
+                }
+
+                // Update the max streak
+                longestStreak = Math.Max(longestStreak, currentStreak);
+            }
+
+            return longestStreak;
+        }
+
+        static void Main(string[] args)
+        {
+            // Input array
+            int[] nums = { 100, 4, 200, 1, 3, 2 };
+
+            // Output using HashSet-based method
+            Console.WriteLine(LongestConsecutive(nums)); // Output: 4
+
+            // Output using sort-based method
+            Console.WriteLine(LongestConsecutive2(nums)); // Output: 4
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HashSetProblems/FindLongestConsecutiveSequence/FindLongestConsecutiveSequence/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FindLongestConsecutiveSequence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FindLongestConsecutiveSequence")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c46a3b1c-1c56-47b7-a887-276fbe360774")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
