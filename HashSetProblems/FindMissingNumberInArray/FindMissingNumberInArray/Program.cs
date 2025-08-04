59fbe792dd9c2d02e302947941bb161cbe9c5a43
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-04 06:24:10 +0300

    Add Find missing Number in Array Problem

---
+++ HashSetProblems/FindMissingNumberInArray/FindMissingNumberInArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMissingNumberInArray
+{
+    internal class Program
+    {
+        static int FindMissingNumber(int[]nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            int n = nums.Length;
+
+
+            for (int i = 0; i <= n; i++)
+            {
+                if(!set.Contains(i))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 200, 0, 1 };
+
+            Console.WriteLine(FindMissingNumber(nums));
+
+            Console.ReadKey();
+        }
+    }
+}
