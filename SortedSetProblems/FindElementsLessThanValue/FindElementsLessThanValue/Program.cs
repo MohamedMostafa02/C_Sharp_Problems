b567e6e9131d5c15a6340792d8d4f09c3de4355d
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-05 20:22:27 +0300

    Add Find Elements Less Than Value Problem

---
+++ SortedSetProblems/FindElementsLessThanValue/FindElementsLessThanValue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindElementsLessThanValue
+{
+    internal class Program
+    {
+        // This method returns all elements in the SortedSet that are less than the given value.
+        static IEnumerable<int> ElementsLessThan(SortedSet<int> set, int value)
+        {
+            // GetViewBetween returns a view of the elements between two bounds (inclusive),
+            // so we use int.MinValue to cover all possible values less than (value - 1).
+            return set.GetViewBetween(int.MinValue, value - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            // Create a sorted set with initial values
+            SortedSet<int> sortedSet = new SortedSet<int>() { 1, 2, 3, 4, 5 };
+
+            // Define the value to compare with
+            int value = 4;
+
+            // Print elements from the set that are less than the given value
+            Console.WriteLine(string.Join(", ", ElementsLessThan(sortedSet, value)));
+
+            // Wait for user key press before closing the console window
+            Console.ReadKey();
+        }
+    }
+}
