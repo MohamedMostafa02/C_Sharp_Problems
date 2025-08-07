109084a778987692e25c4aa35cec386578257b53
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-07 09:38:25 +0300

    Add Comments

--- ObservableProblems/DynamicWeatherUpdates/DynamicWeatherUpdates/Program.cs
+++ ObservableProblems/DynamicWeatherUpdates/DynamicWeatherUpdates/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
+using System.Collections.ObjectModel; // Required for ObservableCollection
+using System.Collections.Specialized; // Required for NotifyCollectionChangedEventArgs
 
 namespace DynamicWeatherUpdates
 {
@@ -12,18 +12,22 @@ namespace DynamicWeatherUpdates
     {
         static void Main(string[] args)
         {
+            // Create an observable collection to hold weather update strings
             ObservableCollection<string> weatherUpdates = new ObservableCollection<string>();
 
+            // Subscribe to the CollectionChanged event to respond to additions or removals
             weatherUpdates.CollectionChanged += (sender, e) =>
             {
+                // If a new item is added, print the update to the console
                 if (e.Action == NotifyCollectionChangedAction.Add)
                     Console.WriteLine($"Weather Update: {e.NewItems[0]}");
-
             };
 
+            // Add two sample weather updates to the collection
             weatherUpdates.Add("New York: Sunny, 25°");
             weatherUpdates.Add("London: Rainy, 15°");
 
+            // Wait for user input before closing the program
             Console.ReadKey();
         }
     }
