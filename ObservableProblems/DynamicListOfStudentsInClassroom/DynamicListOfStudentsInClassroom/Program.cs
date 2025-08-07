4c141d95ca6056defbb760d701ab16dab33e2ccd
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-07 07:10:23 +0300

    Add Dynamic List Of Students in Classroom Problem

---
+++ ObservableProblems/DynamicListOfStudentsInClassroom/DynamicListOfStudentsInClassroom/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace DynamicListOfStudentsInClassroom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<string> studnets = new ObservableCollection<string>();
+
+            studnets.CollectionChanged += (sender, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                    Console.WriteLine($"New Student Added: {e.NewItems[0]}");
+                if (e.Action == NotifyCollectionChangedAction.Remove)
+                    Console.WriteLine($"Student Removed: {e.OldItems[0]}");
+            };
+
+            studnets.Add("Alice");
+            studnets.Add("Bob");
+            studnets.Remove("Alice");
+            Console.ReadKey();
+        }
+    }
+}
