698b96ebfa01d3c455e4dab7b6ed3ac0c59ee846
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-07 08:54:39 +0300

    Add Dynamic Weather Updates Problem

---
+++ ObservableProblems/DynamicWeatherUpdates/DynamicWeatherUpdates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace DynamicWeatherUpdates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<string> weatherUpdates = new ObservableCollection<string>();
+
+            weatherUpdates.CollectionChanged += (sender, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                    Console.WriteLine($"Weather Update: {e.NewItems[0]}");
+
+            };
+
+            weatherUpdates.Add("New York: Sunny, 25°");
+            weatherUpdates.Add("London: Rainy, 15°");
+
+            Console.ReadKey();
+        }
+    }
+}
