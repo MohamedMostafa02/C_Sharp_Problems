985df7b58b8735fca5822bfb7f8ff483f52ce366
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-08-07 07:14:31 +0300

    Add Comments To the Code

--- ObservableProblems/DynamicListOfStudentsInClassroom/DynamicListOfStudentsInClassroom/Program.cs
+++ ObservableProblems/DynamicListOfStudentsInClassroom/DynamicListOfStudentsInClassroom/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
+using System.Collections.ObjectModel; // For ObservableCollection
+using System.Collections.Specialized; // For NotifyCollectionChangedEventArgs
 
 namespace DynamicListOfStudentsInClassroom
 {
@@ -12,19 +12,29 @@ namespace DynamicListOfStudentsInClassroom
     {
         static void Main(string[] args)
         {
-            ObservableCollection<string> studnets = new ObservableCollection<string>();
+            // Create a dynamic list of students using ObservableCollection
+            ObservableCollection<string> students = new ObservableCollection<string>();
 
-            studnets.CollectionChanged += (sender, e) =>
+            // Subscribe to the CollectionChanged event to detect when items are added or removed
+            students.CollectionChanged += (sender, e) =>
             {
+                // If a new item was added to the collection
                 if (e.Action == NotifyCollectionChangedAction.Add)
                     Console.WriteLine($"New Student Added: {e.NewItems[0]}");
+
+                // If an item was removed from the collection
                 if (e.Action == NotifyCollectionChangedAction.Remove)
                     Console.WriteLine($"Student Removed: {e.OldItems[0]}");
             };
 
-            studnets.Add("Alice");
-            studnets.Add("Bob");
-            studnets.Remove("Alice");
+            // Add students to the collection (this will trigger the "Add" event)
+            students.Add("Alice");
+            students.Add("Bob");
+
+            // Remove a student from the collection (this will trigger the "Remove" event)
+            students.Remove("Alice");
+
+            // Wait for a key press before closing the program
             Console.ReadKey();
         }
     }
